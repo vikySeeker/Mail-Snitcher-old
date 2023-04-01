f20531658391dc76d926f45c2279cf76b9e0253f
vikySeeker <vikyseeker@gmail.com>
2023-04-01 22:44:56 +0530

    moving to labs

--- __configs/google/config.php
+++ __configs/google/config.php
@@ -12,7 +12,7 @@ session_start();
 // create Client Request to access Google API
 $client = new Google_Client();
 $client->setAuthConfig(__DIR__."/../../credentials.json");
-$client->setRedirectUri("http://localhost/app/index.php");
+$client->setRedirectUri("http://localhost:8000/app/index.php");
 $client->addScope("email");
 $client->addScope("https://www.googleapis.com/auth/gmail.readonly");
 $client->addScope("profile");
--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 require_once __DIR__.'/__configs/google/config.php';
 if (isset($_SESSION['user_token'])) {
-    header("Location: app");
+    header("Location: http://localhost:8080/app/");
 }
 ?>
 <!DOCTYPE html>
@@ -11,38 +11,36 @@ if (isset($_SESSION['user_token'])) {
 	<meta charset="UTF-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>Document</title>
-	<link rel="stylesheet" href="/styles/">
-
+	<title>Mail Snitcher</title>
+	<link rel="stylesheet" href="/styles/style.css">
 </head>
 
 <body>
-
+	<img class="wave" src="/styles/images/SIDE WAVE.png" alt="">
 	<div class="container">
-		<div class="loginpage"></div>
-		<div class="content">
-			<h1> Mail Snitcher</h1>
-			<h4>Welcome To Mail Snitcher...</h4>
-			<div class="btn"><a href="/login/google/" class="google">
-					<button><img src="/styles/images/icons8-google-48.png"><span>Sing In With Google</span>
-					</button>
-				</a>
+		<div class="image">
+			<img src="/styles/images/undraw_newsletter_re_wrob.svg" alt="">
+		</div>
 
-				<a href="#" class="yahoo">
-					<button><img src="/styles/images/icons8-yahoo-48.png"><span>Sing In With Yahoo</span>
-					</button>
-				</a>
+		<div class="login-container">
 
-				<a href="#" class="microsoft">
-					<button><img src="/styles/images/icons8-microsoft-48.png"><span>Sing In With Microsoft</span>
-					</button>
-				</a>
-
+			<img class="avatar" src="/styles/images/undraw_female_avatar_efig.svg" alt="">
+			<h2 class="intro">SIGN IN</h2>
+			<p class="note">Welcome To Mail Snitcher</p>
+			<span class="line"></span>
+			<div class="btn">
+				<button class="icon" onclick="google()"><img src="/styles/images/icons8-google-48.png" alt="">Sign In
+					With Google</button>
+				<button class="icon" onclick="microsoft()"><img src="/styles/images/icons8-microsoft-48.png" alt="">Sign
+					In With Microsoft</button>
+				<button class="icon" onclick="yahoo()"><img src="/styles/images/icons8-yahoo-48.png" alt="">Sign In With
+					yahoo</button>
 			</div>
 
 		</div>
-	</div>
 
+	</div>
+	<script src="/styles/app.js"></script>
 </body>
 
 </html>
